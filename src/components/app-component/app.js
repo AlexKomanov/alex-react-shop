ff11b6149256a32ae35bb4c9b669bf81da427080
Alex <akomanov88@gmail.com>
2021-08-07 12:27:23 +0300

    Refactoring to Components structure

---
+++ src/components/app-component/app.js
@@ -0,0 +1,14 @@
+import Header from "../header-component/header";
+import Products from "../products-component/products";
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Products />
+        </div>
+    );
+};
+
+export default App;
