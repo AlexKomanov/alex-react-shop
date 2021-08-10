d4a73d179c30480b6f3aa90ae78403c160cd3adb
Alex <akomanov88@gmail.com>
2021-08-10 08:00:48 +0300

    Added Spinner Component and CSS

---
+++ src/components/spinner-component/spinner.js
@@ -0,0 +1,13 @@
+import 'spinner.css';
+
+const Spinner = () => {
+    return (
+        <main className="spinner-examples">
+            <div className="example">
+                <span className="smooth spinner"/>
+            </div>
+        </main>
+    );
+};
+
+export default Spinner;
